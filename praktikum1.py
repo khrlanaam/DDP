138e1e082202e805e6f66e262123dbe2ec32d53b
Khoirul Anam <anams908004@gmail.com>
2024-11-01 09:50:02 +0700

    tugas 1 macth case

---
+++ praktikum1.py
@@ -0,0 +1,18 @@
+kendaraan =['GTR', 'mobil', '5000', 'biru', '2']
+print(kendaraan)
+
+kendaraan.append('1M')
+kendaraan.append('Gigi')
+print(kendaraan)
+
+kendaraan.insert(2,'BMW')
+print(kendaraan)
+
+print(type(kendaraan))
+print(type(kendaraan[0]))
+print(type(kendaraan[1]))
+print(type(kendaraan[2]))
+print(type(kendaraan[3]))
+print(type(kendaraan[4]))
+print(type(kendaraan[5]))
+print(type(kendaraan[6]))
